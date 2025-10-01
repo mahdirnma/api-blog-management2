37b7b92841622734d9b3e423fd9977c51d06789a
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-01 18:24:25 +0330

    fill models and create relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable=[
+        'title',
+        'description',
+        'date',
+        'is_active',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
