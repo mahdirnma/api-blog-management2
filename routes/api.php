fa931d61acb3b7fad3e792a511c9f01ad6f71b81
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-01 23:48:50 +0330

    build get all posts process

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('posts', PostController::class);
+Route::post('/auth',LoginController::class)->name('login');
