af8d2b4030a1b91e74a1ab25bcd219b377398a38
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-02 10:02:54 +0330

    build Auth process

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreLoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(StoreLoginRequest $request)
     {
-        //
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json('Authentication failed', 401);
+        }
+        dd(Auth::user());
+        $token=Auth::user()->createToken('authToken')->plainTextToken;
+        return response()->json([
+            'user'=>Auth::user(),
+            'token'=>$token,
+        ]);
     }
 }
